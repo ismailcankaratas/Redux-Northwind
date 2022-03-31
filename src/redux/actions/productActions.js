5cb9248bb9ff331a4a1f8df902f89119c2913e37
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2022-03-31 13:03:17 +0300

    Added saveProduct and error management

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -7,6 +7,55 @@ export function getProductsSuccess(products) {
     }
 }
 
+export function createProductSuccess(product) {
+    return {
+        type: actionTypes.CREATE_PRODUCT_SUCCESS,
+        payload: product
+    }
+}
+
+export function updateProductSuccess(product) {
+    return {
+        type: actionTypes.UPDATE_PRODUCT_SUCCESS,
+        payload: product
+    }
+}
+
+export function saveProductApi(product) {
+    return fetch("https://localhost:3000/products/" + (product.id || ''),
+        {
+            method: product.id ? "PUT" : "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(product)
+        })
+        .then(handleResponse)
+        .catch(handleError);
+}
+
+export function saveProduct(product) {
+    return function (dispatch) {
+        return saveProductApi(product).then(savedProduct => {
+            product.id
+                ? dispatch(updateProductSuccess(savedProduct))
+                : dispatch(createProductSuccess(savedProduct))
+        }).catch(error => { throw error; });
+    }
+}
+
+export async function handleResponse(response) {
+    if (response.ok) {
+        return response.json();
+    }
+
+    const error = await response.text();
+    throw new Error(error);
+}
+
+export function handleError(error) {
+    console.log("Api hatası oluştu.");
+    throw error;
+}
+
 export function getProducts(categoryId) {
     return function (dispatch) {
         let url = "http://localhost:3000/products";
--- src/redux/reducers/productListReducer.js
+++ src/redux/reducers/productListReducer.js
@@ -1,7 +1,7 @@
 import * as actionTypes from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function getListReducer(state = initialState.products, action) {
+export default function productListReducer(state = initialState.products, action) {
     switch (action.type) {
         case actionTypes.GET_PRODUCTS_SUCCESS:
             return action.payload;
